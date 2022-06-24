be7cf4eb035c74c250a1932f87df0fa00fb4875d
seyiadel <seyiadel03@gmail.com>
2022-06-24 09:23:41 +0100

    Initial Zuri Blog

---
+++ blog/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+
+from blog.models import Post
+from django.views.generic import ListView,CreateView,DetailView,UpdateView
+
+# Create your views here.
+class PostListView(ListView):
+    model=Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields ="__all__"
+    success_url=reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model: Post
+
+class PostUpdateView(UpdateView):
+    model:Post
+    fields="__all__"
+    success_url=reverse_lazy("blog:all")
+
+class PostDeleteView(UpdateView):
+    model:Post
+    fields="__all__"
+    success_url=reverse_lazy("blog:all")
